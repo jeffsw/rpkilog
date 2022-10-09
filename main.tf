7b835db826e13d204d5fcb22060b130c66fb4c0c
Jeff Wheeler <jeffsw6@gmail.com>
2022-10-09 16:05:12 -0400

    Move terraform state to S3.  Add .terraform.lock.hcl to repo.

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+    backend "s3" {
+        bucket = "rpkilog-terraform"
+        dynamodb_table = "rpkilog-terraform"
+        key = "main"
+        region = "us-east-1"
+    }
     required_providers {
         aws = {
             source = "hashicorp/aws"
